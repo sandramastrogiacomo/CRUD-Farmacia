4ddf14a6ff92605f81615874749fc3187ad36890
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-03-19 16:23:10 -0300

    Ajustes produto controller

--- src/main/java/com/generation/pharmatech/controller/ProdutoController.java
+++ src/main/java/com/generation/pharmatech/controller/ProdutoController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.pharmatech.model.Produto;
+import com.generation.pharmatech.repository.CategoriaRepository;
 import com.generation.pharmatech.repository.ProdutoRepository;
 
 import jakarta.validation.Valid;
@@ -30,6 +31,9 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll(){
@@ -51,18 +55,28 @@ public class ProdutoController {
 	
 	@PostMapping
 	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+		if (categoriaRepository.existsById(produto.getCategoria().getId()))
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(produtoRepository.save(produto)); 
+		
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
 	}
 	
 	@PutMapping
 	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
-		return produtoRepository.findById(produto.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-			           .body(produtoRepository.save(produto)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); 
+		if(produtoRepository.existsById(produto.getId())) {
+			
+			if(categoriaRepository.existsById(produto.getCategoria().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(produtoRepository.save(produto)); 
+				
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+		}
+		
+		        return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); 
 		        
-	}
+     	}
+	
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
@@ -73,5 +87,7 @@ public class ProdutoController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 		
 		produtoRepository.deleteById(id);
-	}
+	
+}
+
 }
